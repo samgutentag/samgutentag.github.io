50af831b99380eb8116f715116d7ed8a3e79b7b2
Sam Gutentag <samgutentag@me.com>
2014-07-17 17:47:32 -0700

    banner in place

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
 
-	var pageWidth = $(window).width();
-	var pageHeight = $(window).height();
-	$('#contentDiv').append('<div class="pageSize">Page Width: <strong>' + pageWidth + "</strong></div>");
-	$('#contentDiv').append('<div class="pageSize">Page Height: <strong>' + pageHeight + "</strong></div>");
-	
-	$(window).resize(function(){
-		$( ".pageSize" ).remove();
-		pageWidth = $(window).width();
-		pageHeight = $(window).height();
-		$('#contentDiv').append('<div class="pageSize">Page Width: <strong>' + pageWidth + "</strong></div>");
-		$('#contentDiv').append('<div class="pageSize">Page Height: <strong>' + pageHeight + "</strong></div>");
-	});
-});
+}
