716f32c33c856eb22d6ed79b3772ee8bd569c534
Sam Gutentag <samgutentag@me.com>
2014-08-06 13:14:13 -0700

    checkin files

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
-}
+	var pageWidth = $(window).width();
+	var pageHeight = $(window).height();
+	$('#contentDiv').append('<div class="pageSize">Page Width: <strong>' + pageWidth + "</strong></div>");
+	$('#contentDiv').append('<div class="pageSize">Page Height: <strong>' + pageHeight + "</strong></div>");
+	
+	$(window).resize(function(){
+		$( ".pageSize" ).remove();
+		pageWidth = $(window).width();
+		pageHeight = $(window).height();
+		$('#contentDiv').append('<div class="pageSize">Page Width: <strong>' + pageWidth + "</strong></div>");
+		$('#contentDiv').append('<div class="pageSize">Page Height: <strong>' + pageHeight + "</strong></div>");
+	});
+});
