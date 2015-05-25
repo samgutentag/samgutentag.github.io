6e2339dbee9d4897209b60d0754db82ded4ace72
Sam Gutentag <samgutentag@me.com>
2015-05-24 22:43:19 -0700

    updating all pages, cleaning up looks#

--- script.js
+++ script.js
@@ -2,14 +2,34 @@ $(document).ready(function() {
 
 	var pageWidth = $(window).width();
 	var pageHeight = $(window).height();
-	$('#contentDiv').append('<div class="pageSize">Page Width: <strong>' + pageWidth + "</strong></div>");
-	$('#contentDiv').append('<div class="pageSize">Page Height: <strong>' + pageHeight + "</strong></div>");
+
+	pageWidth = $(window).width();
+	pageHeight = $(window).height();
+	var idealWidth = 1000;
+	var fontsize = 100;
+	var percentage = pageWidth / idealWidth;
+	var newFontSize = Math.floor(fontsize * percentage) -1;
+	$(".nameBanner").css("font-size", newFontSize);
+
 	
 	$(window).resize(function(){
-		$( ".pageSize" ).remove();
 		pageWidth = $(window).width();
 		pageHeight = $(window).height();
-		$('#contentDiv').append('<div class="pageSize">Page Width: <strong>' + pageWidth + "</strong></div>");
-		$('#contentDiv').append('<div class="pageSize">Page Height: <strong>' + pageHeight + "</strong></div>");
+		percentage = pageWidth / idealWidth;
+		newFontSize = Math.floor(fontsize * percentage) -1;
+		$(".nameBanner").css("font-size", newFontSize);
+
+
+
+
+
+
+
+
+
+
 	});
+
+
+
 });
